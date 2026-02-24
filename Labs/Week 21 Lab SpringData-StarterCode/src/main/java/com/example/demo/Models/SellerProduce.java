22e6da7643ec10c23ca5811462aae0c35d29cc91
Royson <107987640+DiscLat@users.noreply.github.com>
2026-02-24 14:02:40 +0000

    Get Set update

--- Labs/Week 21 Lab SpringData-StarterCode/src/main/java/com/example/demo/Models/SellerProduce.java
+++ Labs/Week 21 Lab SpringData-StarterCode/src/main/java/com/example/demo/Models/SellerProduce.java
@@ -1,17 +1,18 @@
 package com.example.demo.Models;
 
-import java.util.List;
+import java.math.BigDecimal;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "Seller_Produce")
@@ -21,14 +22,58 @@ public class SellerProduce{
     private Long id;
 
     @ManyToOne(fetch=FetchType.LAZY)
-    @JoinColumn(name="seller_id")
+    @JoinColumn(name="seller_id")//user id
     private User seller;
 
-    @ManyToMany
-    @JoinTable(name="produce_to_seller", joinColumns = @JoinColumn(name="seller_id"), inverseJoinColumns = @JoinColumn(name="produce_id"))
-    private List<Produce_Type> produceTypes;
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name="produce_id")
+    private Produce_Type product;
 
+    @NotBlank
+    Integer quantity;
 
+    @NotNull
+    @Column(name="price",precision = 8, scale = 2)
+    private BigDecimal price;
 
     
+    public long getId(){
+        return id;
+    }
+
+    public void setId(long id){
+        this.id = id;
+    }
+
+    public User getSeller(){
+        return seller;
+    }
+
+    public void setSeller(User seller){
+        this.seller = seller;
+    }
+
+    public Produce_Type getProduct(){
+        return product;
+    }   
+
+    public void setProduct(Produce_Type product){
+        this.product = product;
+    }
+
+    public Integer getQuantity(){
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity){
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getPrice(){
+        return price;
+    }
+
+    public void setPrice(BigDecimal price){
+        this.price = price;
+    }
 }
